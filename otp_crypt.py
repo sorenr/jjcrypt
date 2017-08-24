b7d3271e161741796e19944e05953ab53606c903
Søren Ragsdale <sorenr@users.noreply.github.com>
2017-08-24 12:01:19 +0100

    PEP8

--- otp_crypt.py
+++ otp_crypt.py
@@ -4,48 +4,51 @@ import sys
 import otp_gen
 
 # retrieve the value from the one-time-pad
-def otp_get(otp_fd,index):
-	while True:
-		try:
-			return otp_get.pad[index]
-		except KeyError:
-			# read a line from the pad
-			line = otp_fd.readline()
-			if not line:
-				print "pad too short."
-				sys.exit(-1)
-			# skip comment lines
-			if line.startswith('#'):
-				continue
-			line = line.split()
-			# add the values into the pad
-			while line:
-				otp_get.pad[int(line[0])] = int(line[1])
-				line = line[3:]
+
+
+def otp_get(otp_fd, index):
+    while True:
+        try:
+            return otp_get.pad[index]
+        except KeyError:
+            # read a line from the pad
+            line = otp_fd.readline()
+            if not line:
+                print "pad too short."
+                sys.exit(-1)
+            # skip comment lines
+            if line.startswith('#'):
+                continue
+            line = line.split()
+            # add the values into the pad
+            while line:
+                otp_get.pad[int(line[0])] = int(line[1])
+                line = line[3:]
 otp_get.pad = {}
 
-def otp_crypt(direction,otp_fd,message):
-	message = message.upper()
-	cryptext = ""
-	for i in range(len(message)):
-		if message[i] not in otp_gen.letters:
-			cryptext = cryptext + message[i]
-			continue
-		n = otp_get(otp_fd,i)
-		cc = ( ord(message[i])-ord('A')+(n*direction) ) % 26
-		cryptext = cryptext + chr(cc+ord('A'))
-	print cryptext
 
-if __name__=="__main__":
-	if len(sys.argv)<=1:
-		print "Usage:",sys.argv[0],"[ -c | -d ] otp.txt message"
+def otp_crypt(direction, otp_fd, message):
+    message = message.upper()
+    cryptext = ""
+    for i in range(len(message)):
+        if message[i] not in otp_gen.letters:
+            cryptext = cryptext + message[i]
+            continue
+        n = otp_get(otp_fd, i)
+        cc = (ord(message[i])-ord('A')+(n*direction)) % 26
+        cryptext = cryptext + chr(cc+ord('A'))
+    print cryptext
 
-	if sys.argv[1][1]=='c':
-		val = -1
-	elif sys.argv[1][1]=='d':
-		val = 1
-	else:
-		print "Pleas select encrypt (-c) or decrypt (-d)."
-		sys.exit(-1)
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print "Usage:", sys.argv[0], "[ -c | -d ] otp.txt message"
 
-	otp_crypt(val,open(sys.argv[2]),sys.argv[3])
+    if sys.argv[1][1] == 'c':
+        val = -1
+    elif sys.argv[1][1] == 'd':
+        val = 1
+    else:
+        print "Pleas select encrypt (-c) or decrypt (-d)."
+        sys.exit(-1)
+
+    otp_crypt(val, open(sys.argv[2]), sys.argv[3])
--- otp_gen.py
+++ otp_gen.py
@@ -8,36 +8,37 @@ import math
 
 letters = string.ascii_uppercase
 
+
 def otp_gen(numbers):
-	print "# Increment each character"
-	print "# by the value in the pad."
-	print "# Wrap Z->A for overflow."
-	print "# Example: W + 6 = C"
-	just = int(math.log(numbers)/math.log(10))+1
-	pad = []
+    print "# Increment each character"
+    print "# by the value in the pad."
+    print "# Wrap Z->A for overflow."
+    print "# Example: W + 6 = C"
+    just = int(math.log(numbers)/math.log(10))+1
+    pad = []
 
-	# generate the pad
-	for i in range(numbers):
-		n = ord(os.urandom(1))%len(letters)
-		c = letters[n]
-		v = string.join( [ str(i).rjust(just), str(ord(c)-ord('A')).rjust(2) ] )
-		pad.append(v)
+    # generate the pad
+    for i in range(numbers):
+        n = ord(os.urandom(1)) % len(letters)
+        c = letters[n]
+        v = string.join([str(i).rjust(just), str(ord(c)-ord('A')).rjust(2)])
+        pad.append(v)
 
-	# write out the values in columns
-	cols = 4
-	rows = int(math.ceil(len(pad)/float(cols)))
-	for r in range(rows):
-		for c in range(cols):
-			p = c*rows+r
-			if p < len(pad):
-				print pad[p],
-			if c < cols:
-				print ' ',
-		print
+    # write out the values in columns
+    cols = 4
+    rows = int(math.ceil(len(pad)/float(cols)))
+    for r in range(rows):
+        for c in range(cols):
+            p = c*rows+r
+            if p < len(pad):
+                print pad[p],
+            if c < cols:
+                print ' ',
+        print
 
 if __name__ == "__main__":
-	if len(sys.argv)<=1:
-		print "Usage:",sys.argv[1],"[chars required] > otp.txt"
-		sys.exit(-1)
+    if len(sys.argv) <= 1:
+        print "Usage:", sys.argv[1], "[chars required] > otp.txt"
+        sys.exit(-1)
 
-	otp_gen(int(sys.argv[1]))
+    otp_gen(int(sys.argv[1]))
